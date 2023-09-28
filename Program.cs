9fa575f10a76356da20fbba196cbd03d8f27a5ee
Esex <elseverclash57@gmail.com>
2023-09-28 04:24:17 +0400

    'done'

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 23;
+
+        if (num <= 1)
+        {
+            Console.WriteLine("number is not a prime");
+        }
+        else
+        {
+            bool isPrime = true;
+            for (int i = 2; i * i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine("number is a prime");
+            }
+            else
+            {
+                Console.WriteLine("number is not a prime.");
+            }
+        }
+    }
+}
